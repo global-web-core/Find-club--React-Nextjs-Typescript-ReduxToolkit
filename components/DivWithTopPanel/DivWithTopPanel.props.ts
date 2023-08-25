40c6cdf9e2b7e4e2c2b5177c366b64ded2827466
uuu8001uuu <83807818+uuu8001uuu@users.noreply.github.com>
2023-08-25 11:39:27 +0200

    Changed types for component DivWithTopPanel

--- components/DivWithTopPanel/DivWithTopPanel.props.ts
+++ components/DivWithTopPanel/DivWithTopPanel.props.ts
@@ -2,4 +2,6 @@ import { DetailedHTMLProps, HTMLAttributes, ReactNode} from 'react';
 
 export interface DivWithTopPanelProps extends DetailedHTMLProps<HTMLAttributes<HTMLElement>, HTMLElement> {
 	children: ReactNode;
+	topPanel: JSX.Element;
+	meetingsListMain: boolean;
 }
